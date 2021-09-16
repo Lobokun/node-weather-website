d9cd604758bb299dc4fcf766ee6579e0dd71f1ba
lobo <lobokun@gmail.com>
2021-09-16 12:11:55 -0700

    fix undefined property

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibG9ib2t1biIsImEiOiJja3JjcDAyaWEzN25jMnVuM3V5cHFwYXFiIn0.MnA131GCHCE6P4FCc3oonA&limit=1'
 
-   request({url, json: true}, (error, {body}) => {
+   request({url, json: true}, (error, {body} = {}) => {
 
        if(error){
            callback('Unable to connect to location services!', undefined)
